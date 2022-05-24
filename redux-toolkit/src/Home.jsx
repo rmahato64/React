6cbb2cb319e4b56bdf653945b074cb03da5d4884
Rakesh Mahato <rmahato64@gmail.com>
2022-05-24 13:30:17 +0300

    react projects

---
+++ redux-toolkit/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Home = () => {
+    const value = 20;
+    
+  return (
+    <div>
+        <h2>{value}</h2>
+        <button>Increament</button>
+        <button>Decreament</button>
+    </div>
+  )
+}
+
+export default Home
